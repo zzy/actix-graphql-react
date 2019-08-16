ab5eaddd976954c4c55b9cc5ea54e6070f56e02e
zzy <9809920@qq.com>
2019-08-16 21:10:43 +0800

    :star:

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+pub mod schema;
+pub mod models;
+
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+use self::models::{NewPost, Post};
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &PgConnection, title: &str, body: &str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
