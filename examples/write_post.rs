ea44a8ae41f414446f42867c99b9d0d8dce1a73a
zzy <9809920@qq.com>
2019-08-17 15:02:49 +0800

    写入测试

--- examples/write_post.rs
+++ examples/write_post.rs
@@ -7,10 +7,10 @@ use std::io::{stdin, Read};
 fn main() {
     let connection = establish_connection();
 
-    println!("What would you like your title to be?");
+    println!("请输入：");
     let mut title = String::new();
     stdin().read_line(&mut title).unwrap();
-    let title = &title[..(title.len() - 1)]; // Drop the newline character
+    let title = &title[..(title.len() - 1)];
     println!("\nOk! Let's write {} (Press {} when finished)\n", title, EOF);
     let mut body = String::new();
     stdin().read_to_string(&mut body).unwrap();
