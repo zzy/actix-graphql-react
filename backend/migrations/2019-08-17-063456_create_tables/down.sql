9d7ab66fbd7de2c15c41aa05fbe09ce181331127
zzy <9809920@qq.com>
2020-03-08 18:54:17 +0800

    -

--- backend/migrations/2019-08-17-063456_create_tables/down.sql
+++ backend/migrations/2019-08-17-063456_create_tables/down.sql
@@ -1,5 +1,5 @@
 DROP TRIGGER set_updated_at ON users;
 DROP TABLE IF EXISTS users;
 
-DROP TRIGGER set_updated_at ON sites;
-DROP TABLE IF EXISTS sites;
+DROP TRIGGER set_updated_at ON projects;
+DROP TABLE IF EXISTS projects;
--- backend/migrations/2019-08-17-063456_create_tables/up.sql
+++ backend/migrations/2019-08-17-063456_create_tables/up.sql
@@ -1,24 +1,29 @@
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
-	email VARCHAR(70) UNIQUE NOT NULL,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR UNIQUE NOT NULL,
+	username VARCHAR NOT NULL,
 	password VARCHAR NOT NULL,
-	bio TEXT,
-	avatar VARCHAR,
+	-- bio TEXT,
+	-- avatar VARCHAR,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 SELECT diesel_manage_updated_at('users');
 
-CREATE TABLE sites (
+CREATE TABLE projects (
   id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
-  title VARCHAR NOT NULL,
-  body TEXT NOT NULL,
+  -- country VARCHAR,
+  -- industry VARCHAR NOT NULL,
+  subject VARCHAR NOT NULL,
+  -- tags VARCHAR NOT NULL,
+  website VARCHAR,
+  -- body TEXT NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   published BOOLEAN NOT NULL DEFAULT 'f',
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-SELECT diesel_manage_updated_at('sites');
+SELECT diesel_manage_updated_at('projects');
