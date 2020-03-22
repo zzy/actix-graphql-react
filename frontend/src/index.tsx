c8dd0e64c5f12d883b70e98fadb7912fd43558bb
zzy <9809920@qq.com>
2020-03-22 18:00:19 +0800

    -

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Pages from './pages';
+import Login from './pages/login';
+import { resolvers, typeDefs } from './resolvers';
+import injectStyles from './styles';
+
+// Set up our apollo-client to point at the server we created
+// this can be local or a remote endpoint
+const cache = new InMemoryCache();
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'https://api.budshome.com/gql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  }),
+  resolvers,
+  typeDefs,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+/**
+ * Render our app
+ * - We wrap the whole app with ApolloProvider, so any component in the app can
+ *    make GraphqL requests. Our provider needs the client we created above,
+ *    so we pass it as a prop
+ * - We need a router, so we can navigate the app. We're using Reach router for this.
+ *    The router chooses between which component to render, depending on the url path.
+ *    ex: localhost:3000/login will render only the `Login` component
+ */
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
+ReactDOM.render(
+
+  <ApolloProvider client={client}>
+    <IsLoggedIn />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
